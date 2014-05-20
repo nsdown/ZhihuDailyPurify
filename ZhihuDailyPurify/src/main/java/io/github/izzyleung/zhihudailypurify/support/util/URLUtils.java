885c3d5a98f809baaf04dc56d17c9a2756be20a4
Izzy Leung <izzyliang@gmail.com>
2014-05-20 22:51:36 +0800

    Little enhance

--- ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/support/util/URLUtils.java
+++ ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/support/util/URLUtils.java
@@ -1,12 +1,8 @@
 package io.github.izzyleung.zhihudailypurify.support.util;
 
-public final class URLUtils {
-    public static final String ZHIHU_DAILY_BEFORE_URL = "http://news.at.zhihu.com/api/2/news/before/";
-    public static final String ZHIHU_DAILY_PURIFY_HEROKU_BEFORE_URL = "http://zhihu-daily-purify.herokuapp.com/raw/";
-    public static final String ZHIHU_DAILY_PURIFY_SAE_BEFORE_URL = "http://zhihudailypurify.sinaapp.com/raw/";
-    public static final String SAE_SEARCH_URL = "http://zhihudailypurify.sinaapp.com/search/";
-
-    private URLUtils() {
-
-    }
+public interface URLUtils {
+    String ZHIHU_DAILY_BEFORE_URL = "http://news.at.zhihu.com/api/2/news/before/";
+    String ZHIHU_DAILY_PURIFY_HEROKU_BEFORE_URL = "http://zhihu-daily-purify.herokuapp.com/raw/";
+    String ZHIHU_DAILY_PURIFY_SAE_BEFORE_URL = "http://zhihudailypurify.sinaapp.com/raw/";
+    String SAE_SEARCH_URL = "http://zhihudailypurify.sinaapp.com/search/";
 }
