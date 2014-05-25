ef2d69f38854bb21c32f8461fe32c29fb9da2d84
Izzy Leung <izzyliang@gmail.com>
2014-05-25 10:57:08 +0800

    OkHtpp in use instead of Apache Http Components

--- ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/task/BaseDownloadTask.java
+++ ZhihuDailyPurify/src/main/java/io/github/izzyleung/zhihudailypurify/task/BaseDownloadTask.java
@@ -1,42 +1,15 @@
 package io.github.izzyleung.zhihudailypurify.task;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import io.github.izzyleung.zhihudailypurify.support.lib.MyAsyncTask;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-public abstract class BaseDownloadTask <Params, Progress, Result> extends MyAsyncTask<Params, Progress, Result> {
+public abstract class BaseDownloadTask<Params, Progress, Result> extends MyAsyncTask<Params, Progress, Result> {
     protected String downloadStringFromUrl(String url) throws IOException {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
-
-        InputStream in = null;
-        StringBuilder str = new StringBuilder();
-
-        try {
-            in = client.execute(request).getEntity().getContent();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                str.append(line);
-            }
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException ignored) {
-
-                }
-            }
-        }
-
-        return str.toString();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        return client.newCall(request).execute().body().string();
     }
 }
